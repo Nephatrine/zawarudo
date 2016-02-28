edf22acdff745d0346417b4ee96fb794a9a24594
Daniel Wolf <nephatrine@pos.nxp>
2016-02-28 03:07:38 -0500

    Created Basic Mapping Module

---
+++ monomap.hpp
@@ -0,0 +1,131 @@
+#ifndef MONOMAP_HPP
+#define MONOMAP_HPP
+
+#include <cassert>
+#include <string>
+
+#include "config.hpp"
+#include "stb_image_write.h"
+
+#include <iostream>
+
+namespace zw
+{
+	template<int X, int Y>
+	struct monomap
+	{
+		monomap()
+			: data {0}, min( 0 ), max( 255 ), offset( 64 )
+		{}
+		
+		void setPixel( const real_t lat, const real_t lon, const unsigned char value )
+		{
+			int x = ( lon + 180.0 ) / 360.0 * ( X - 1 );
+			int y = ( 180.0 - ( lat + 90.0 ) ) / 180.0 * ( Y - 1 );
+			
+			assert( x > 0 && x < X );
+			assert( y > 0 && y < Y );
+			
+			std::size_t pixel = x + ( y * X );
+			
+			assert( pixel < X * Y );
+			
+			data[pixel] = ( 255.0 - offset ) * ( value - min ) / ( max - min ) + offset;
+		}
+		
+		void fill()
+		{
+			unsigned char newstate[X * Y] = {0};
+			int nofills = -1;
+			int oldfills = 0;
+			int div, tot;
+			size_t pixel;
+			
+			while ( nofills != 0 && nofills != oldfills )
+			{
+				oldfills = nofills;
+				nofills = 0;
+				
+				for ( int x = 0; x < X; ++x )
+					for ( int y = 0; y < Y; ++y )
+					{
+						pixel = x + y * X;
+						
+						if ( data[pixel] )
+							newstate[pixel] = data[pixel];
+						else
+						{
+							div = 0;
+							tot = 0;
+							
+							if ( x > 0 && data[pixel - 1] )
+							{
+								tot += data[pixel - 1];
+								++div;
+							}
+							else if ( x == 0 && data[pixel + 511] )
+							{
+								tot += data[pixel + 511];
+								++div;
+							}
+							
+							if ( x < X - 1 && data[pixel + 1] )
+							{
+								tot += data[pixel + 1];
+								++div;
+							}
+							else if ( x == X - 1 && data[pixel - 511] )
+							{
+								tot += data[pixel - 511];
+								++div;
+							}
+							
+							if ( y > 0 && data[pixel - X] )
+							{
+								tot += data[pixel - X];
+								++div;
+							}
+							else if ( y == 0 && data[pixel + X * ( Y - 1 )] )
+							{
+								tot += data[pixel + X * ( Y - 1 )];
+								++div;
+							}
+							
+							if ( y < Y - 1 && data[pixel + X] )
+							{
+								tot += data[pixel + X];
+								++div;
+							}
+							else if ( y == Y - 1 && data[x] )
+							{
+								tot += data[x];
+								++div;
+							}
+							
+							if ( div > 0 )
+								newstate[pixel] = tot / div;
+							else
+								++nofills;
+						}
+					}
+					
+				for ( std::size_t i = 0; i < X * Y; ++i )
+					data[i] = newstate[i];
+			}
+		}
+		
+		void write( const std::string &fileName ) const
+		{
+			stbi_write_png( fileName.c_str(), X, Y, 1,
+			                reinterpret_cast<const void *>( data ), 0 );
+		}
+		
+		unsigned char data[X *Y];
+		unsigned char min;
+		unsigned char max;
+		unsigned char offset;
+	};
+}
+
+#endif
+
--- vector.hpp
+++ vector.hpp
@@ -5,6 +5,10 @@
 #include <cmath>
 #include <ostream>
 
+#define DEG2RAD(x) ((x) * 3.1415926535897932384626433832795 / 180.0)
+#define RAD2DEG(x) ((x) / 3.1415926535897932384626433832795 * 180.0)
+#define PI 3.1415926535897932384626433832795
+
 namespace math
 {
 #ifdef MATH_REAL_T
@@ -37,6 +41,17 @@ namespace math
 		real_t dotProduct( const vector &v ) const
 		{
 			return x * v.x + y * v.y + z * v.z;
+		}
+		
+		real_t latitude() const
+		{
+//			return RAD2DEG(std::asin( z / magnitude() ));
+			return RAD2DEG( std::atan2( z, std::sqrt( x * x + y * y ) ) );
+		}
+		
+		real_t longitude() const
+		{
+			return RAD2DEG( std::atan2( y, x ) );
 		}
 		
 		real_t magnitude() const
@@ -106,6 +121,15 @@ namespace math
 	inline vector operator-( const vector &a, const vector &b ) {return vector( a ) -= b;}
 	inline vector operator*( const vector &v, const real_t s ) {return vector( v ) *= s;}
 	inline vector operator/( const vector &v, const real_t s ) {return vector( v ) /= s;}
+	
+	// Lat/Long
+	inline vector LLC2VEC( const real_t lat, const real_t lon,
+	                       const real_t radius = 1.0 )
+	{
+		return vector( std::cos( DEG2RAD( lat ) ) * std::cos( DEG2RAD( lon ) ),
+		               std::cos( DEG2RAD( lat ) ) * std::sin( DEG2RAD( lon ) ),
+		               std::sin( DEG2RAD( lat ) ) ) * radius;
+	}
 	
 	inline std::ostream &operator<<( std::ostream &s, const vector &v )
 	{
