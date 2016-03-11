681d21e10d95d60ddd64edd8b166670a619f24b2
Daniel Wolf <nephatrine@pos.nxp>
2016-03-11 14:50:41 -0500

    incorporate plotting & projection improvements

--- zawarudo.cpp
+++ zawarudo.cpp
@@ -37,13 +37,26 @@ int main( int argc, const char *argv[] )
 	opt.add( "world", 0, 1, 0, "[STRING] World Filename Slug", "-w", "--world" );
 	
 	// Mapping Options
-	opt.add( "equirectangular", 0, 1, 0, "[STRING] Map -> Projection\n  "
-	         "braun-stereo    - Braun Stereographic\n  "
-	         "equirectangular - Equirectangular (SP X)\n  "
-	         "gall-stereo     - Gall Stereographic\n  "
+	opt.add( "equirect", 0, 1, 0, "[STRING] Map -> Projection\n  "
+	         "aitoff          - Aitoff\n  "
+	         "behrmann        - Behrmann Equal-Area\n  "
+	         "braun           - Braun Stereographic\n  "
+	         "cea             - Cylindrical Equal-Area (SP X)\n  "
+	         "equirect        - Equirectangular (SP X)\n  "
+	         "gall            - Gall Stereographic\n  "
+	         "gall-peters     - Gall Orthographic\n  "
+	         "hammer          - Hammer\n  "
+	         "hobo-dyer       - Hobo-Dyer Equal-Area\n  "
+	         "lambert         - Lambert Equal-Area\n  "
+	         "kavrayskiy      - Kavrayskiy VII\n  "
 	         "mercator        - Mercator\n  "
-	         "miller-cyl      - Miller Cylindrical\n  "
-	         "plate-carree    - Plate Caree", "-m", "--map" );
+	         "miller          - Miller Cylindrical\n  "
+	         "ortelius        - Ortelius Oval\n  "
+	         "orthographic    - Orthographic\n  "
+	         "plate-carree    - Plate Carree\n  "
+	         "sinusoidal      - Sinusoidal\n  "
+	         "wagner          - Wagner VI\n  "
+	         "winkel          - Winkel III", "-m", "--map" );
 	opt.add( "0.0", 0, 1, 0, "[DEGREES] Map -> Standard Parallel", "--parallel" );
 	opt.add( "0.0", 0, 1, 0, "[DEGREES] Map -> Prime Meridian", "--meridian" );
 	opt.parse( argc, argv );
@@ -112,12 +125,16 @@ int main( int argc, const char *argv[] )
 	std::string mapType;
 	opt.get( "-m" )->getString( mapType );
 	
-	real_t parallel = 0;
+	real_t parallel = ( mapType == "winkel" ) ? RAD2DEG( std::acos(
+	                      2 / M_PI ) ) : 0;
 	real_t meridian = 0;
 	
-	opt.get( "--parallel" )->getFloat( parallel );
-	opt.get( "--meridian" )->getFloat( meridian );
-	
+	if ( opt.isSet( "--parallel" ) )
+		opt.get( "--parallel" )->getFloat( parallel );
+		
+	if ( opt.isSet( "--meridian" ) )
+		opt.get( "--meridian" )->getFloat( meridian );
+		
 	if ( parallel < 0 || parallel >= 90 )
 	{
 		std::cerr << "Standard parallel must be within range 0 <= SP < 90." <<
@@ -198,41 +215,201 @@ int main( int argc, const char *argv[] )
 	}
 	
 	//
-	// Create Maps
+	// Cylindrical Projections
 	//
 	
-	std::unique_ptr<projection::base> mapView;
+	if ( mapType == "behrmann" )
+	{
+		mapType = "cea";
+		parallel = 30.0;
+	}
+	else if ( mapType == "gall-peters" )
+	{
+		mapType = "cea";
+		parallel = 45.0;
+	}
+	else if ( mapType == "hobo-dyer" )
+	{
+		mapType = "cea";
+		parallel = 37.5;
+	}
+	else if ( mapType == "lambert" )
+	{
+		mapType = "cea";
+		parallel = 0;
+	}
 	
-	if ( mapType == "braun-stereo" )
-		mapView = std::unique_ptr<projection::base>( new projection::stereographic(
-		              1 ) );
-	else if ( mapType == "gall-stereo" )
-		mapView = std::unique_ptr<projection::base>( new projection::stereographic(
-		              2 ) );
+	//
+	// Equirectangular Projections
+	//
+	
+	if ( mapType == "plate-carree" )
+	{
+		mapType = "equirect";
+		parallel = 0;
+	}
+	
+	//
+	// Create Map Projections
+	//
+	
+	std::string mapTag = mapType;
+	int mapNumB = parallel * 60.0;
+	int mapNumA = mapNumB / 60;
+	int mapNumC = meridian;
+	mapNumB -= mapNumA * 60;
+	
+	int hemisphere = 180;
+	
+	using proj_ptr = std::unique_ptr<projection::base>;
+	proj_ptr view, undistorted;
+	
+	if ( mapType == "aitoff" )
+	{
+		view = proj_ptr( new projection::aitoff() );
+		mapNumA = mapNumB = 0;
+	}
+	else if ( mapType == "braun" )
+	{
+		view = proj_ptr( new projection::stereographic( 1 ) );
+		mapNumA = mapNumB = 0;
+	}
+	else if ( mapType == "cea" )
+		view = proj_ptr( new projection::equalarea( DEG2RAD( parallel ) ) );
+	else if ( mapType == "gall" )
+	{
+		view = proj_ptr( new projection::stereographic( 2 ) );
+		mapNumA = mapNumB = 0;
+	}
+	else if ( mapType == "hammer" )
+	{
+		view = proj_ptr( new projection::hammer() );
+		mapNumA = mapNumB = 0;
+	}
+	else if ( mapType == "kavrayskiy" )
+	{
+		view = proj_ptr( new projection::kavrayskiy() );
+		mapNumA = mapNumB = 0;
+	}
 	else if ( mapType == "mercator" )
-		mapView = std::unique_ptr<projection::base>( new projection::mercator() );
-	else if ( mapType == "miller-cyl" )
-		mapView = std::unique_ptr<projection::base>( new projection::miller() );
-	else if ( mapType == "plate-carree" )
-		mapView = std::unique_ptr<projection::base>( new projection::equirectangular(
-		              0 ) );
+	{
+		view = proj_ptr( new projection::mercator() );
+		mapNumA = mapNumB = 0;
+	}
+	else if ( mapType == "miller" )
+	{
+		view = proj_ptr( new projection::miller() );
+		mapNumA = mapNumB = 0;
+	}
+	else if ( mapType == "ortelius" )
+	{
+		view = proj_ptr( new projection::ortelius() );
+		mapNumA = mapNumB = 0;
+	}
+	else if ( mapType == "orthographic" )
+	{
+		view = proj_ptr( new projection::orthographic( DEG2RAD( parallel ) ) );
+		undistorted = proj_ptr( new projection::orthographic( 0 ) );
+		hemisphere = 90;
+	}
+	else if ( mapType == "sinusoidal" )
+	{
+		view = proj_ptr( new projection::sinusoidal() );
+		mapNumA = mapNumB = 0;
+	}
+	else if ( mapType == "wagner" )
+	{
+		view = proj_ptr( new projection::wagner() );
+		mapNumA = mapNumB = 0;
+	}
+	else if ( mapType == "winkel" )
+		view = proj_ptr( new projection::winkel( DEG2RAD( parallel ) ) );
 	else
-		mapView = std::unique_ptr<projection::base>( new projection::equirectangular(
-		              DEG2RAD( parallel ) ) );
-		              
-	mapView->meridian( DEG2RAD( meridian ) );
+	{
+		view = proj_ptr( new projection::equirectangular( DEG2RAD( parallel ) ) );
+		mapTag = "equirect";
+	}
 	
-	plotter::gs mapRegion( mapView->aspect(), 768, 512 );
+	view->meridian( DEG2RAD( meridian ) );
+	
+	//
+	// Draw Region Map
+	//
+	
+	plotter::gs mapRegion( view->aspect(), 768, 512 );
 	mapRegion.inputRange( 0, std::min<real_t>( cells, REGION_LIMIT - 1.0 ) );
 	
 	for ( cell_size_t c = 0; c < cells; ++c )
-		mapRegion.plot( mapView->convert( geodesic[c].v ), geodesic[c].region );
+		if ( view->valid( coord( geodesic[c].v ) ) )
+			mapRegion.plot( view->convert( geodesic[c].v ), geodesic[c].region );
+			
+	//
+	// Create Map Border
+	//
+	
+	if ( undistorted )
+	{
+		for ( int spin = -900; spin < 900; ++spin )
+		{
+			mapRegion.bold( undistorted->convert( coord( DEG2RAD( -hemisphere ),
+			                                      DEG2RAD( spin / 10.0 ) ) ), 1 );
+			mapRegion.bold( undistorted->convert( coord( DEG2RAD( hemisphere ),
+			                                      DEG2RAD( spin / 10.0 ) ) ), 1 );
+		}
+	}
+	else
+	{
+		for ( int spin = -900; spin < 900; ++spin )
+		{
+			mapRegion.bold( view->convert( coord( DEG2RAD( -hemisphere + meridian ),
+			                                      DEG2RAD( spin / 10.0 ) ) ), 1 );
+			mapRegion.bold( view->convert( coord( DEG2RAD( hemisphere + meridian ),
+			                                      DEG2RAD( spin / 10.0 ) ) ), 1 );
+		}
+	}
+	
+	//
+	// Fill Map Gaps
+	//
+	
+	mapRegion.fill();
+	
+	//
+	// Draw Graticules
+	//
+	
+	for ( int graticule = -90; graticule < 90; graticule += 15 )
+		for ( int spin = -540; spin < 540; ++spin )
+		{
+			coord c( DEG2RAD( spin / 3.0 + meridian ), DEG2RAD( graticule ) );
+			
+			if ( view->valid( c ) ) mapRegion.set( view->convert( c ) , 1 );
+		}
 		
+	for ( int graticule = -180; graticule < 180; graticule += 15 )
+		for ( int spin = -540; spin < 540; ++spin )
+		{
+			coord c( DEG2RAD( graticule + meridian ), DEG2RAD( spin / 6.0 ) );
+			
+			if ( view->valid( c ) ) mapRegion.set( view->convert( c ) , 1 );
+		}
+		
+	//
+	// Write Map
+	//
+	
 	{
 		std::stringstream fileMap;
-		fileMap << nameOut << "_" << iterations << "_region.png";
+		fileMap << nameOut << "_" << iterations << "_region." << mapTag;
+		
+		if ( mapNumA > 0 || mapNumB > 0 ) fileMap << "-p" << mapNumA;
+		
+		if ( mapNumB > 0 ) fileMap << "-" << mapNumB;
+		
+		if ( mapNumC > 0 ) fileMap << "-m" << mapNumC;
+		
+		fileMap << ".png";
 		std::cout << "saving map " << fileMap.str() << std::endl;
-		mapRegion.fill();
 		mapRegion.write( fileMap.str() );
 	}
 	
